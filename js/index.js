1a55eb910a5cc3ee1246abf91ca2527ef14b89f9
YOUR_FULL NAME_HERE <your@email.here.com>
2020-10-01 09:08:48 +1000

    working properly

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
 
-const ticTac = {
+$(document).ready(function(){
+  let selectedItemsX = [];
+  let selectedItemsO = [];
+$('#playAgain').on('click',function(){
+  selectedItemsX = [];
+  selectedItemsO = [];
+  $('#messageBox').text("");
+  $('.square').removeClass("x");
+  $('.square').removeClass("o");
+  player = "x";
+  let gameStatus = "start" ;
+  $('h2').text(gameStatus);
+  startGame();
+});
 
- winConditions : [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ],
- playerSelection : [2,4,6],
- checkWin : function(){
- const sameChar=[];
-   for (var d = 0; d < this.winConditions.length; d++) {
-      const arr1 = this.winConditions[d];
-      const arr2 =  this.playerSelection;
+const startGame = function (){
+$('.square').on('click',function(){
+if (player === "x") {
+$(this).addClass("x");
+selectedItemsX.push(this.id);
+checkWin(selectedItemsX);
+player = "o";
+}else{
+$(this).addClass("o");
+selectedItemsO.push(this.id);
+checkWin(selectedItemsO);
+player = "x";
+    }
+  });
+}
+
+
+let player = "";
+const  checkWin = function(playerSelection){
+const   winConditions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+   let sameChar = [];
+   for (var d = 0; d < winConditions.length; d++) {
+      const arr1 = winConditions[d];
+      const arr2 =  playerSelection;
       const finalArray = [];
-
-     for (var i = 0; i < arr1.length; i++) {
+       for (var i = 0; i < arr1.length; i++) {
        for (var j = 0; j < arr2.length; j++) {
          if (arr1[i] == arr2[j]) {
            finalArray.push(arr1[i]);
@@ -19,14 +48,16 @@ const ticTac = {
      }
     sameChar.push(finalArray.length);
    }
-
 let result = "";
 if (sameChar.includes(3)) {
   result = "win";
 } else {
   result="loose";
 }
-console.log(result);
+if (result === "win") {
+  $('#messageBox').text(`Winner is : ${player} `);
+   $('.square').off("click");
+}
+}
 
-}
-}
+});
---
+++ js/ticJQ.js
@@ -0,0 +1,32 @@
+//
+// $(document).ready(function(){
+// let selectedItemsX = [];
+// let selectedItemsO = [];
+//
+// $('#playAgain').on('click',function(){
+//   player = "x";
+//   let gameStatus = "start" ;
+//   $('h2').text(gameStatus);
+//   startGame();
+// });
+//
+// const startGame = function (){
+//
+//   $('.square').on('click',function(){
+//
+//     if (player === "x") {
+//       $(this).text('X');
+//       selectedItemsX.push(this.id);
+//         checkWin(selectedItemsX);
+//         player = "o";
+//
+//     }else{
+//       $(this).text('O');
+//       selectedItemsO.push(this.id);
+//         checkWin(selectedItemsO);
+//         player = "x";
+//         console.log(index.result);
+//     }
+//   });
+// }
+// });
