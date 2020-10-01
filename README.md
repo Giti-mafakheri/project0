c8921afe1e1ae4d7fe2c9106a7d5d1439310ee5f
YOUR_FULL NAME_HERE <your@email.here.com>
2020-10-01 17:29:21 +1000

    implementing round tracks

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tic Tac Toe
+## The game keeps track of rounds.
+**To start playing the game you have to choose your player then press _start_ button.**
+
+
+
+## technologies
+ * JavaScript
+ * HTML
+ * CSS
+ * jQuery
--- js/index.js
+++ js/index.js
@@ -2,26 +2,43 @@
 $(document).ready(function(){
   let selectedItemsX = [];
   let selectedItemsO = [];
+  let winnerX=[];
+  let winnerO=[];
+  let player = "";
+  $('#so').on('click',function(){
+      player = "o";
+  });
+  $('#sx').on('click',function(){
+    //debugger;
+      player = "x";
+     //$('#sx').off("click")
+  });
+
+  //on clicking play button game resets and the method startGame is called
 $('#playAgain').on('click',function(){
+  //debugger;
   selectedItemsX = [];
   selectedItemsO = [];
   $('#messageBox').text("");
   $('.square').removeClass("x");
   $('.square').removeClass("o");
-  player = "x";
+  if (player == "") {
+    alert("Choose a player!");
+    throw 'null player';
+  }
   let gameStatus = "start" ;
-  $('h2').text(gameStatus);
   startGame();
 });
-
+// this function replys to the player click and sets the choosen items array
 const startGame = function (){
 $('.square').on('click',function(){
+//debugger;
 if (player === "x") {
 $(this).addClass("x");
 selectedItemsX.push(this.id);
 checkWin(selectedItemsX);
 player = "o";
-}else{
+}else if(player === "o"){
 $(this).addClass("o");
 selectedItemsO.push(this.id);
 checkWin(selectedItemsO);
@@ -30,11 +47,12 @@ player = "x";
   });
 }
 
+//this function compares the selected array with the winConditions array to see if winConditions contain selected array or not
 
-let player = "";
 const  checkWin = function(playerSelection){
 const   winConditions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
    let sameChar = [];
+   let result = "";
    for (var d = 0; d < winConditions.length; d++) {
       const arr1 = winConditions[d];
       const arr2 =  playerSelection;
@@ -48,15 +66,26 @@ const   winConditions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], 
      }
     sameChar.push(finalArray.length);
    }
-let result = "";
+
 if (sameChar.includes(3)) {
   result = "win";
-} else {
+   } else {
   result="loose";
 }
+
 if (result === "win") {
   $('#messageBox').text(`Winner is : ${player} `);
-   $('.square').off("click");
+  if (player == 'x') {
+    winnerX.push(1);
+    $('.tablex').append('<td class="setsq"></td>');
+  } else {
+    winnerO.push(1);
+    $('.tableo').append('<td class="setsq" ></td>');
+  }
+  $('.square').off("click");
+   player = "";
+  // debugger;
+   throw 'round finished';
 }
 }
 
